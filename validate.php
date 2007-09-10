71bb0c174e28884eedd5c24432c73213f40b7e0c
gnovaro <gnovaro@03da6df5-8639-0410-899c-37835a462aff>
2007-09-10 18:31:10 +0000

    Add adslash in input text validate.php Fix folder image path in tree.php

--- validate.php
+++ validate.php
@@ -5,16 +5,13 @@ E-mail: gnovaro@gmail.com
 URL: http://www.novarsystems.com.ar
 Purpouse: Generic validation hardcode
 */
-	$user = $_POST["txtUserName"]; //adslash
-	$pass = $_POST["txtPass"];
-	if ( $user == "admin" &&  $pass == "admin")
-	{
+	$user = addslashes($_POST["txtUserName"]);  
+	$pass = addslashes($_POST["txtPass"]);
+	if ( $user == "admin" &&  $pass == "admin"){
 		session_start();
 		$_SESSION["login"] = true;
 		header("Location: tree.php");	
-	}
-	else
-	{
+	}else{
 		header("Location: index.php");
 	}//if
 ?>
