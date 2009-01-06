4f8d058c3cf1493ce7bd5f55b63a1acc9d1a43e5
gnovaro <gnovaro@03da6df5-8639-0410-899c-37835a462aff>
2009-01-06 20:44:50 +0000

    Remove close ?> tag replace " x '

--- config_sample.php
+++ config_sample.php
@@ -1,12 +1,12 @@
 <?php  
 /** 
+* Configuration File
 * @author  Gustavo Novaro <gnovaro@gmail.com> 
-* @version 1.17
+* @version 1.18
 * http://fileexplorerxp.googlecode.com
 */ 
-$sConfig["VERSION"] = "1.17"; 
-$sConfig["USER"] = "admin";  
-$sConfig["PASS"] = "d033e22ae348aeb5660fc2140aec35850c4da997";  
-$sConfig["TIME_ZONE"] = "UTC";  
-define("URL","http://localhost/fileexplorerxp");   //whithout final slash
-?>
+$sConfig['VERSION'] 	= '1.18'; 
+$sConfig['USER'] 		= 'admin';  
+$sConfig['PASS'] 		= 'd033e22ae348aeb5660fc2140aec35850c4da997';  
+$sConfig['TIME_ZONE'] 	= 'UTC';  
+define('URL','http://localhost/fileexplorerxp');   //whithout final slash
