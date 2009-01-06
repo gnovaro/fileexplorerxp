728ac0510a61a1d57a837d717803a28fb129129e
gnovaro <gnovaro@03da6df5-8639-0410-899c-37835a462aff>
2009-01-06 21:50:38 +0000

    remove close tag update generation config.php

--- trunk/dosetup.php
+++ trunk/dosetup.php
@@ -1,9 +1,8 @@
 <?php
 /**
+* Setup Process
 * @author Gustavo Novaro <gnovaro@gmail.com>
-* @version 1.25
-* Change Log
-* - Change md5 to sha1
+* @version 1.27
 */
 require("./error_handler.php");	
 require("./function.php");
@@ -13,28 +12,27 @@ if ($_POST["txtUserAdmin"]){
 	$sPass = sha1($_POST["txtPassAdmin"]);
 	$sUrl = $_POST["txtUrl"];
 	$sTimeZone = (isset($_POST["php_zone"]))? $_POST["php_zone"] : "";
-	$sVerion = "1.25";
+	$sVersion = '1.28';
 	
 	$oFile = fopen("config.php","w+");
 	$sLineBreak = " \n";
 	
 	$sContent = '<?php '.$sLineBreak;
 	$sContent .='/**'.$sLineBreak;
+	$sContent .='* Configuration File'.$sLineBreak;
 	$sContent .='* @author Gustavo Novaro <gnovaro@gmail.com>'.$sLineBreak;
 	$sContent .='* @version '.$sVerion.$sLineBreak;
 	$sContent .='* http://fileexplorerxp.googlecode.com'.$sLineBreak;
 	$sContent .='*/'.$sLineBreak;
-	$sContent .='$sConfig["VERSION"] = "'.$sVerion.'";'.$sLineBreak;
-	$sContent .='$sConfig["USER"] = "'.$sUser.'"; '.$sLineBreak;
-	$sContent .='$sConfig["PASS"] = "'.$sPass.'"; '.$sLineBreak;
+	$sContent .='$sConfig["VERSION"]   = "'.$sVersion.'";'.$sLineBreak;
+	$sContent .='$sConfig["USER"]      = "'.$sUser.'"; '.$sLineBreak;
+	$sContent .='$sConfig["PASS"]      = "'.$sPass.'"; '.$sLineBreak;
 	$sContent .='$sConfig["TIME_ZONE"] = "'.$sTimeZone.'"; '.$sLineBreak;
 	$sContent .='define("URL","'.$sUrl.'"); '.$sLineBreak;
-	$sContent .="?>";
 	
 	fwrite($oFile,$sContent);
 	fclose($oFile);
-	chmod("config.php",444); //Change file permisions to read only
-	$_SESSION["MSG"] = "Setup Sucess";
-	goto('index.php');
+	chmod('config.php',444); //Change file permisions to read only
+	$_SESSION['__MSG__'] = 'Setup Sucess';
+	redirect('index.php');
 }//if
-?>
