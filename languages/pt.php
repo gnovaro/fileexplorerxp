7fe579b6275af06e37bf077ab2b72e83877b5306
gnovaro <gnovaro@03da6df5-8639-0410-899c-37835a462aff>
2009-07-24 18:01:06 +0000

    Se agrega archivo de lenguage para el portugese

---
+++ languages/pt.php
@@ -0,0 +1,58 @@
+<?php
+/**
+* @author	Gustavo Novaro <gnovaro@gmail.com>
+* @version 1.04
+*/
+	$CONTENT['TITLE'] 			= '::: File Explorer XP :::';
+	$CONTENT['USER'] 			= '';
+	$CONTENT['PASSWORD'] 		= 'Sena';
+	$CONTENT['DETAILS'] 		= '';
+	$CONTENT['OBJECTS'] 		= '';
+	$CONTENT['SPACE_FREE'] 		= '';
+	$CONTENT['SPACE_TOTAL'] 	= '';
+	$CONTENT['NAME'] 			= '';
+	$CONTENT['SIZE'] 			= '';
+	$CONTENT['LAST_MODIFY'] 	= '';
+	$CONTENT['OWNER'] 			= ''; 
+	$CONTENT['GROUP'] 			= '';
+	$CONTENT['PERMISSIONS'] 	= '';
+	$CONTENT['GO'] 				= '';
+	$CONTENT['FILE_TASK'] 		= ''; 
+	$CONTENT['NEW_FILE'] 		= ''; 
+	$CONTENT['NEW_FOLDER'] 		= '';  
+	$CONTENT['UPLOAD_FILE'] 	= ''; 
+	$CONTENT['CONTROL_PANEL'] 	= ''; 
+	$CONTENT['BT_CANCEL'] 		= ''; 
+	$CONTENT['BT_UPLOAD'] 		= '';
+	$CONTENT['RENAME_FILE'] 	= ''; 
+	$CONTENT['DELETE_FILE']	 	= ''; 
+	$CONTENT['DETAILS'] 		= '';
+	$CONTENT['EXIT'] 			= '';
+	$CONTENT['PATH'] 			= '';
+	$CONTENT['FILE_SUCESS'] 		= '';	
+	$CONTENT['FILE_FAIL'] 			= '';
+	$CONTENT['FILE_ADD'] 			= '';
+	$CONTENT['DELETE_DIR_SUCESS'] 	= '';
+	$CONTENT['DELETE_DIR_FAIL'] 	= '';
+	$CONTENT['DELETE_FILE_SUCESS'] 	= '';
+	$CONTENT['DELETE_FILE_FAIL'] 	= '';
+	$CONTENT['RENAME_FILE_SUCESS'] 	= '';
+	$CONTENT['RENAME_FILE_FAIL'] 	= '';
+	$CONTENT['EXIT'] 				= '';
+	$CONTENT['EDIT'] 				= '';
+	$CONTENT['DOWNLOAD'] 			= '';
+	$CONTENT['DELETE'] 				= '';
+	$CONTENT['RENAME'] 				= '';
+	$CONTENT['COMPRESS'] 			= '';
+	$CONTENT['CHOOSE_FILE'] 		= '';
+	$CONTENT['UPLOAD_SUCESS'] 		= '';
+ 	$CONTENT['UPLOAD_FAIL'] 		= '';
+	$CONTENT['HELP'] 				= '';
+	$CONTENT['SUBJECT'] 			= '';
+	$CONTENT['SUBMIT'] 				= '';
+	$CONTENT['BACK'] 				= '';
+	$CONTENT['NEXT'] 				= '';
+	$CONTENT['LANGUAGE'] 			= '';
+	$CONTENT['REPORT_BUG'] 			= '';
+	$CONTENT['USER_ACCOUNTS'] 		= '';
+	$CONTENT['SETUP'] 				= '';
