d1824603ac205932a1949d6f61aca5946f0565bb
gnovaro <gnovaro@03da6df5-8639-0410-899c-37835a462aff>
2010-07-29 18:18:42 +0000

    add utf-8 head

--- trunk/edit.php
+++ trunk/edit.php
@@ -35,7 +35,7 @@ if(isset($_POST['H_FILE_NAME']))
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title><?php echo $CONTENT['TITLE'];?></title>
     <link rel="stylesheet" type="text/css" href="<?php echo URL?>/style.css" />
     <script src="<?php echo URL?>/codepress/codepress.js" type="text/javascript"></script>
