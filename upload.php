d0829b342850aea3d5c431a28cf6e4b34f081b5b
gnovaro <gnovaro@03da6df5-8639-0410-899c-37835a462aff>
2010-07-29 18:18:42 +0000

    add utf-8 head

--- upload.php
+++ upload.php
@@ -32,7 +32,7 @@ if(!isset($_SESSION["login"]))
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title><?php echo $CONTENT["TITLE"];?></title>
     <link rel="stylesheet" type="text/css" href="<?php echo URL?>/fileexplorer.css" />
     <script type="text/javascript" src="<?php echo URL?>/js/function.js"></script>
