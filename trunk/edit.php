ca58a09593b0308ce63634cb9f4112ae8fbe07b5
gnovaro <gnovaro@03da6df5-8639-0410-899c-37835a462aff>
2011-05-25 06:04:54 +0000

    minor fixes php coding style, doctype html5

--- trunk/edit.php
+++ trunk/edit.php
@@ -1,15 +1,15 @@
 <?php
 /**
 * @author Gustavo Novaro <gnovaro@gmail.com>
-* @version 1.39
-* URL: http://gustavonovaro.blogspot.com
+* @version 1.77
+* URL: http://gustavonovaro.com.ar
 * File: edit.php
 * Purpose: Edit text files
 */
 
 require('./config.php');
 require('./languages/es.php');
-require('error_handler.php');	
+require('error_handler.php');
 
 //Security check
 session_start();
@@ -24,45 +24,44 @@ if(isset($_POST['H_FILE_NAME']))
 	$sFile = $_POST['H_FILE_NAME'];
 	$sPath = $_SESSION['path'];
 	$sPath = $sPath.DIRECTORY_SEPARATOR.$sFile;	
-							
+
 	$sContent = $_POST['content'];
 	$fp = fopen($sPath,"w+");
 	fwrite($fp,$sContent);
 	fclose($fp);
-	
 }//if
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html>
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title><?php echo $CONTENT['TITLE'];?></title>
-    <link rel="stylesheet" type="text/css" href="<?php echo URL?>/style.css" />
-    <script src="<?php echo URL?>/codepress/codepress.js" type="text/javascript"></script>
-    <script src="<?php echo URL?>/js/function.js" type="text/javascript"></script>
-    <meta name="robots" content="NOINDEX, NOFOLLOW, NOCACHE, NOARCHIVE" />
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title><?php echo $CONTENT['TITLE'];?></title>
+	<link rel="stylesheet" type="text/css" href="<?php echo URL?>/fileexplorer.css" />
+	<script src="<?php echo URL?>/codepress/codepress.js" type="text/javascript"></script>
+	<script src="<?php echo URL?>/js/function.js" type="text/javascript"></script>
+	<meta name="robots" content="NOINDEX, NOFOLLOW, NOCACHE, NOARCHIVE" />
 </head>
 
 <body>
 <?php
-/*if(isset($_POST["H_FILE_NAME"]))	
+/*if(isset($_POST['H_FILE_NAME']))
 	echo "Datos guardados satifactoriamente";*/
 ?>
 	<h2>Editar Archivo:</h2>
 	<form id="frmEditor" action="" method="post" onsubmit="myCpWindow.toggleEditor();">
 	<div id="editor" align="center">
-    <table>
-    <tr>
-	    <td>
-        <?php
-            if(isset($_GET['file']))
-                $sFile = $_GET['file'];
-            
-            if(isset($_POST['H_FILE_NAME']))
-                $sFile = $_POST['H_FILE_NAME'];
-                
-            $sPath = $_SESSION['path'];
-            $sPath = $sPath.DIRECTORY_SEPARATOR.$sFile;
+	<table>
+	<tr>
+		<td>
+		<?php
+			if(isset($_GET['file']))
+				$sFile = $_GET['file'];
+
+			if(isset($_POST['H_FILE_NAME']))
+				$sFile = $_POST['H_FILE_NAME'];
+				
+			$sPath = $_SESSION['path'];
+			$sPath = $sPath.DIRECTORY_SEPARATOR.$sFile;
 			//
 			$path_parts = pathinfo($sPath);
 			$ext = $path_parts['extension'];
@@ -106,25 +105,25 @@ if(isset($_POST['H_FILE_NAME']))
 						break;
 				}
 
-            if (is_file($sPath)){
-                $sContent = file_get_contents($sPath);
-            }//is_file
-        ?>
-        <textarea name="content" id="myCpWindow" class="codepress <?php echo $language;?> linenumbers-on" cols="80" rows="40"><?php echo $sContent;?></textarea>
-        </td>
+			if (is_file($sPath)){
+				$sContent = file_get_contents($sPath);
+			}//is_file
+		?>
+		<textarea name="content" id="myCpWindow" class="codepress <?php echo $language;?> linenumbers-on" cols="80" rows="40"><?php echo $sContent;?></textarea>
+		</td>
 	</tr>
-    <tr>        
-		<td>        
-	   	<div style="text-align:right">
-        <input type="button" name="btCancel" value="Cancelar" onclick="redirect('tree.php');" />
-        <input type="submit" name="btSend" id="btSend" value="Guardar" />
-        </div>
-        </td>
-	</tr>        
-    </table>
-    <input type="hidden" name="H_FILE_NAME" id="H_FILE_NAME" value="<?php echo $sFile?>" />
+	<tr>
+		<td>
+		<div style="text-align:right">
+		<input type="button" name="btCancel" value="Cancelar" onclick="redirect('tree.php');" />
+		<input type="submit" name="btSend" id="btSend" value="Guardar" />
+		</div>
+		</td>
+	</tr>
+	</table>
+	<input type="hidden" name="H_FILE_NAME" id="H_FILE_NAME" value="<?php echo $sFile?>" />
 
 	</div>
-    </form>
+	</form>
 </body>
 </html>
