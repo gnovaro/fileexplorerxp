650f57f43443127f082f1503ae6051ce1dff1a12
gnovaro <gnovaro@03da6df5-8639-0410-899c-37835a462aff>
2011-05-25 06:08:29 +0000

    php coding style minor fixes

--- validate.php
+++ validate.php
@@ -2,8 +2,8 @@
 /**
  * Generic validation hardcode process
  * @author Gustavo Novaro <gnovaro@gmail.com>
- * @version 1.74
- * URL: http://gustavonovaro.blogspot.com
+ * @version 1.77
+ * URL: http://gustavonovaro.com.ar
  *
  */
 	require('./config.php');
@@ -14,7 +14,7 @@
 	if ( $user == $config['USER'] &&  $pass == $config['PASS'])
 	{
 		$_SESSION['login'] = true;
-		redirect('tree.php');	
+		redirect('tree.php');
 	}else{
 		$_SESSION['__MSG__'] = 'Invalid user or password';
 		redirect('index.php');
